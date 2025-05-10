2e576fd77cde8978449a8c43bfa04e2adee97a64
markohautala <markohautala@live.se>
2025-05-10 23:31:36 +0200

    feat: enhance GalleryPage layout with skeleton loading and styling

--- vue-project/src/components/GalleryPage.vue
+++ vue-project/src/components/GalleryPage.vue
@@ -1,10 +1,33 @@
-<!-- src/views/GalleryPage.vue -->
 <template>
-  <div>
+  <div class="gallery-page">
     <h1>Gallery Page</h1>
+
+    <n-space justify="center" align="center" wrap>
+      <n-skeleton
+        v-for="n in 3"
+        :key="n"
+        round
+        width="380px"
+        height="300px"
+        :repeat="2"
+        animated
+        :style="{
+          borderRadius: '3%',
+          margin: '5px',
+          marginTop: '25px',
+        }"
+      />
+    </n-space>
   </div>
 </template>
 
 <script setup>
-// No script required for basic page
+// No script logic needed for now
 </script>
+
+<style scoped>
+.gallery-page {
+  padding: 2rem;
+  text-align: center;
+}
+</style>
